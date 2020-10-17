cc708d9569bff678a7441d75b669f78ac2f7c5c7
Umeraftab7747 <umeraftab7747@gmail.com>
2020-10-16 20:13:19 -0700

    master

---
+++ index.js
@@ -0,0 +1,24 @@
+const redux = require("redux");
+const store = redux.createStore;
+
+const BuyBook = "Buy Book";
+
+const intialState = {
+  numberOfbooks: 10,
+};
+
+function BuyBook() {
+  return {
+    type: BuyBook,
+    info: "My First Redux Code",
+  };
+}
+const reducer = (state = intialState, action) => {
+  if (action.type === "Buy Book") {
+    return {
+      numberOfbooks: numberOfbooks + 1,
+    };
+  } else {
+    return state;
+  }
+};
